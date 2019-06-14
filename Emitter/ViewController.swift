32968f26a7504c6276c1c68a74eef50242689345
Liliya <lilyeka@gmail.com>
2019-06-15 01:26:19 +0500

    Initial Commit

---
+++ Emitter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Emitter
+//
+//  Created by Лилия Левина on 15/06/2019.
+//  Copyright © 2019 Лилия Левина. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
